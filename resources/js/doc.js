7edca68dd88a4e1952c969765f8c00e338d31d3d
axeldotdev <axelcharpentier0@icloud.com>
2022-05-19 01:40:44 +0200

    fix issue tooltip copy code

--- resources/js/doc.js
+++ resources/js/doc.js
@@ -6,17 +6,3 @@ import Alpine from 'alpinejs';
 window.Alpine = Alpine;
 
 Alpine.start();
-
-document.addEventListener('alpine:init', () => {
-    Alpine.magic('tooltip', (el) => (message) => {
-        let instance = tippy(el, { content: message, trigger: 'manual' });
-
-        instance.show();
-
-        setTimeout(() => {
-            instance.hide();
-
-            setTimeout(() => instance.destroy(), 150);
-        }, 2000);
-    });
-});
